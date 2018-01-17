962259a0579799281a021d72b4e0bb5af439e92d
Jim Stricker <jmstricker93@gmail.com>
2018-01-17 18:34:06 -0500

    got die component to appropriate size, got images rendering properly

--- client/src/components/Die.js
+++ client/src/components/Die.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 export const Die = ({die, index}) =>{
+  const dieImageURL=`images/6-sided-die/side-${die.value}.png`
+
   return(
     <div className="die" key={index}>
-      {die.value} - {index}
+      <img className="die-image" height="100" width="100" src={dieImageURL} alt="die side 6" />
+      <p>{die.value} - {index}</p>
     </div>
   )
 
