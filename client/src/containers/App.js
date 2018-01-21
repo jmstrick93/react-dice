6c1521ec143c1dc5f6c033831dac857899dfcbcb
Jim Stricker <jmstricker93@gmail.com>
2018-01-21 16:58:28 -0500

    wrapped content of App component in bootstrap container div

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -9,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className='container-fluid'>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to ReactDice</h1>
@@ -17,6 +18,7 @@ class App extends Component {
           To get started, click "add a die."
         </p>
         <DiceListContainer />
+        </div>
       </div>
     );
   }
