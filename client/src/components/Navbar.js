b9775f886b2b055550d21381d88e9bf5907cb443
Jim Stricker <jmstricker93@gmail.com>
2018-01-22 12:32:53 -0500

    added rudamentary bootstrap navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,8 +2,24 @@ import React from 'react';
 
 export const Navbar = () =>{
   return (
-    <nav className='navbar navbar-expand'>
-      Navbar
+    <nav className='navbar navbar-expand-sm'>
+      <a className='navbar-brand' href='#'>ReactDice</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsingNavbar" aria-controls="collapsingNavbar" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsingNavbar">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item active">
+            <a class="nav-link" href="/">Dice</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/history">Roll History</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/settings">Settings</a>
+          </li>
+        </ul>
+      </div>
     </nav>
   )
 }
