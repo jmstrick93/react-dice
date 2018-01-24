e840d99a861c970ea518252c86c69ee23a4fa79e
Jim Stricker <jmstricker93@gmail.com>
2018-01-24 08:50:03 -0500

    dice rolling now dispatches post request to API

--- client/src/containers/DiceListContainer.js
+++ client/src/containers/DiceListContainer.js
@@ -3,10 +3,35 @@ import { DiceList } from "../components/DiceList";
 import { connect } from "react-redux";
 import { bindActionCreators } from 'redux';
 import { addDie, rollDice } from '../actions/diceActions';
+import {postRollHistory} from '../actions/rollHistoryActions';
 
 //only want dice list responsible for how many dice there are and calculating roll total.
 
+
+
 class DiceListContainer extends Component {
+
+  componentDidUpdate(prevProps, prevState){
+    let lengthChange
+    let sidesChange
+    prevProps.dice.length === this.props.dice.length ? lengthChange = false : lengthChange = true;
+
+    if (lengthChange === false && this.props.dice.length > 0){
+
+      let index = 0
+      for (const die of this.props.dice){
+        debugger;
+        if (die.sides !== prevProps.dice[index].sides) {
+          sidesChange = true;
+          break;
+        } else {
+          sidesChange = false;
+          index++
+        };
+      };
+    }
+    sidesChange === false && lengthChange === false ? postRollHistory(this.props.dice) : null
+  }
 
   render(){
     const { dice, addDie, rollDice} = this.props
